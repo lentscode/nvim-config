81608f0ae5e0ab810bac30d580e0f08e66e452db
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2026-05-28 15:34:53 +0200

    feat: add `nvim-surround`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -119,6 +119,11 @@ require("lazy").setup({
         "neovim/nvim-lspconfig",
     },
 	},
+	{
+    "kylechui/nvim-surround",
+    version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+	},
 
 
   -- spec = {
