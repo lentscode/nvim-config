04a70d96d1fd030b345fae56a6e497a1676c5c51
Over Write <111151429+lentscode@users.noreply.github.com>
2026-07-07 10:55:35 +0200

    feat: add dependencies

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -121,6 +121,15 @@ require("lazy").setup({
     version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
 	},
+	{
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+	},
 
 
   -- spec = {
